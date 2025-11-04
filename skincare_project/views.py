8e90b3ac8d552a30bf41741c4dd30af787ca1d66
RJChoe <120413939+RJChoe@users.noreply.github.com>
2025-11-04 11:56:45 -0700

    views.py updated home by adding render

--- skincare_project/views.py
+++ skincare_project/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     #return HttpResponse("Welcome to the Skincare Home Page!")
-
+    return render(request)
+    
 def user(request):
     #return HttpResponse("User Allergy Input Page")
     return render(request, 'user.html')
