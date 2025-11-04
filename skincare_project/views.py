b18d6c3e02d3ea931f29266932c6e1b889dc304d
RJChoe <120413939+RJChoe@users.noreply.github.com>
2025-11-04 11:42:15 -0700

    views.py update return render instead of HttpResponse; home.html update main

--- skincare_project/views.py
+++ skincare_project/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to the Skincare Home Page!")
+    #return HttpResponse("Welcome to the Skincare Home Page!")
 
 def user(request):
-    return HttpResponse("User Allergy Input Page")
+    #return HttpResponse("User Allergy Input Page")
+    return render(request, 'user.html')
 
 def product(request):
-    return HttpResponse("Product Ingredient Input Page")
+    #return HttpResponse("Product Ingredient Input Page")
+    return render
 
